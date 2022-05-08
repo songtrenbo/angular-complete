993af044687c876e79da1bd3216177df2878d266
Nguyen Ly <songtrenbo1@gmail.com>
2022-05-08 15:13:23 +0700

    Buoi 08 Add, Update, Delete recipe

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,32 +1,43 @@
 import { EventEmitter, Injectable } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
-import {Recipe} from './recipe.model';
-import {ShoppingListService} from './../shopping-list/shopping-list.service';
+import { Recipe } from './recipe.model';
+import { ShoppingListService } from './../shopping-list/shopping-list.service';
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root',
 })
-export class RecipeService{
-    recipeSelected = new EventEmitter<Recipe>();
-    private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://assets.bonappetit.com/photos/5c2f8fe26558e92c8a622671/1:1/w_2700,h_2700,c_limit/bolognese-1.jpg',
-        [
-            new Ingredient('Meat', 1),
-            new Ingredient('French Fries', 20)
-        ]),
-        new Recipe('A Test Recipe 2', 'This is simply a test 2', 'https://assets.bonappetit.com/photos/5c2f8fe26558e92c8a622671/1:1/w_2700,h_2700,c_limit/bolognese-1.jpg',
-        [
-            new Ingredient('Buns', 2),
-            new Ingredient('Meat', 1)
-        ])
-      ];
-      constructor(private slService: ShoppingListService) {}
-      getRecipe(index: number){
-          return this.recipes[index];
-      }
-      getRecipes() {
-        return this.recipes.slice();
-        }
-      addIngredientsToShoppingList(ingredients: Ingredient[]) {
-        this.slService.addIngredients(ingredients);
-        }
+export class RecipeService {
+  recipeSelected = new EventEmitter<Recipe>();
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://assets.bonappetit.com/photos/5c2f8fe26558e92c8a622671/1:1/w_2700,h_2700,c_limit/bolognese-1.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
+    ),
+    new Recipe(
+      'A Test Recipe 2',
+      'This is simply a test 2',
+      'https://assets.bonappetit.com/photos/5c2f8fe26558e92c8a622671/1:1/w_2700,h_2700,c_limit/bolognese-1.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
+    ),
+  ];
+  constructor(private slService: ShoppingListService) {}
+  getRecipes() {
+    return this.recipes;
+  }
+  getRecipe(index: number) {
+    return this.recipes[index];
+  }
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
+  }
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+  }
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+  }
+  deleteRecipe(index: number) {
+    this.recipes.splice(index, 1);
+    }
 }
